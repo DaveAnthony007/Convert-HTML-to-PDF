2328d18864a0d521ca02af900f46245f02e9265f
unknown <bcamaster@P090MA121.intra.bca.co.id>
2020-09-22 14:15:00 +0700

    generate PDF from HTML table

--- js/main.js
+++ js/main.js
@@ -9,10 +9,22 @@ $(document).ready(function() {
         var doc = new jsPDF()
 
         doc.fromHTML($("#target").html(), 15, 15, {
-            "width":170,
+            "width":100,
             "elementHandlers":specialElemetHandler
         })
         doc.save("sample-file.pdf")
     })
 
+    // $("#generate-table").on("click", function () {
+    //     $("#example").tableHTMLExport({ type: "pdf", filename: "sample.pdf" });
+    //   });
+
+    $("#generate-table").on("click", () => {
+        var doc = new jsPDF()
+        doc.autoTable({
+            html:'#example'
+        })
+
+        doc.save("sample-table-temp1.pdf")
+    })
 })
